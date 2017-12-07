2ebdd17103f08755eb121628e81b744077066556
lim996 <simonestaffa96@gmail.com>
2017-12-07 18:58:43 +0100

    app.js example nodejs server to control registration and login script

---
+++ app.js
@@ -0,0 +1,93 @@
+/*
+   Questo è un web server da eseguire con NodeJS, da cmd > node app.js.
+   Rimane in ascolto su https://localhost:3000, per cui riceve le richieste get/post su quell'indirizzo.
+   In prospettiva dovrà essere sempre attivo su un ipotetico server da affittare.
+*/
+
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Questa funzione viene chiamata in risposta ad una richiesta post (vedi sotto "app.post").
+// Prende i dati del body della post e dopo essersi connesso al database prova ad inserirli.
+function register(body){
+    // connessione al databse
+    MongoClient.connect("mongodb://simonestaffa:VqhfwYZVnY8XzEjU@parkidleusers-shard-00-00-ertqo.mongodb.net:27017,parkidleusers-shard-00-01-ertqo.mongodb.net:27017,parkidleusers-shard-00-02-ertqo.mongodb.net:27017/Users?replicaSet=ParkIdleUsers-shard-0&ssl=true&authSource=admin", function(err, db) {
+      if(err) { return console.dir(err); }
+      console.log("Succesfully Connected");
+      // scelgo la collection in cu inserire (es. 'utenti')
+      var collection = db.collection('utenti');
+      // documento da inviare che sarà inserito
+      var user = {
+          'name': body.name,
+          'last_name': body.last_name,
+          'username': body.username,
+          'email': body.email,
+          'password': body.password,
+          'birth_date': body.birth_date
+      };
+      // inserimento nella collection 'utenti' (vedi sopra)
+      // la gestione dell'unicità dei campi username ed email è gestita completamente da MongoDb
+      // infatti ho impostato quei campi come 'index' (similar a primary key) in tal modo
+      // se inserisco un duplicato lancia un'eccezione che viene catturata
+      collection.insert(user, function(err,result){
+          if(err){
+              // 'err' è un JSONObject da cui prendo il campo 'message' che contiene informazioni sull'errore lanciato
+              // cercando 'username' in err.message so che il messaggio d'errore si riferisce ad un username duplicato
+              // string.indexOf('esempio') ritorna -1 se la parola 'esempio' non è presente
+              if(err.message.indexOf('username') != -1){
+                  console.log("Username already exists");
+              }
+              // stessa cosa ma ricerco la parola 'email'
+              if(err.message.indexOf('email') != -1){
+                  console.log("Email already exists");
+              }
+              return false;
+          }else{
+              console.log("Insert Completed!");
+              db.close();
+              return true;
+          }
+      });
+    });
+}
+
+// rimango in ascolto in attesa di ricevere una POST e definisco il comportamento da eseguire all'arrivo di una richiesta
+// in questo caso se ricevo una POST su "https://localhost:3000/register" sarà eseguito il codice qui di seguito
+// POST che viene mandata dal form di registrazione nel file 'contactus.html' al momento della submit, impostando action="https://localhost:3000/register"
+app.post('/register', function (req, res) {
+    if(register(req.body) == true){
+        console.log("Registration success! Redirecting to homepage...");
+        res.send('Data received:\n' + JSON.stringify(req.body));
+        //window.location.replace("index.html"); //redirect page
+    }
+    else{
+        console.log("Registration failed!");
+        res.send('Registration Failed!');
+        //window.location.replace("contactus.html");
+    }
+});
+
+// stessa cosa ma in questo caso gestisco il caso del login
+// POST mandata dal form di Login nel modal (vedi navbar) al momento della submit, impostando action="https://localhost:3000/login"
+// TODO
+app.post('/login', function (req, res) {
+    MongoClient.connect("mongodb://simonestaffa:VqhfwYZVnY8XzEjU@parkidleusers-shard-00-00-ertqo.mongodb.net:27017,parkidleusers-shard-00-01-ertqo.mongodb.net:27017,parkidleusers-shard-00-02-ertqo.mongodb.net:27017/Users?replicaSet=ParkIdleUsers-shard-0&ssl=true&authSource=admin", function(err, db) {
+        if(err) { return console.dir(err); }
+        if(db.collection.find({"username":req.body.username,"password":req.body.password}).limit(1) != null){
+            //window.location.replace(window.location.href);
+            return true;
+        }
+        console.log("Incorrect username or password");
+        db.close();
+        return false;
+    });
+});
+
+// imposto la porta e l'IP su cui rimango in ascolto
+app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0' );
